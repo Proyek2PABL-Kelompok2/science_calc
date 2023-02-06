33f2848a54edfa40093149afc0951360b5dedb9e
Coz <117976022+CozRangers@users.noreply.github.com>
2023-02-06 10:36:10 +0700

    ini main pangkat

---
+++ vico.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int x,y,z;
+	int total = 1;
+	
+	printf("Masukan angka yang akan dipangkatkan :");
+	scanf("%i", &y);
+	printf("Masukan pangkat :");
+	scanf("%i",&z);
+	
+	for(x=1; x<=z; x++)
+	{
+		total=total*y;
+	}
+	
+	printf("Hasilnya adalah %d", total);
+	
+	return 0;
+}

6198f016b12eb3cce6e4941d2023fcb3cbc83ad0
thisisjackii <jaxsix06@gmail.com>
2023-02-06 12:17:16 +0700

    Changing includes and implementing references

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "difa.h"
-// #include "fathia qurrata.h"
+#include "fathia.h"
 #include "jacky.h"
 #include "najwan.h"
 #include "vico.h"
@@ -13,6 +13,10 @@ int main(){
     printf("\nmasukkan sebuah operasi : ");
     scanf("%f%c%f", &a, &op, &b);
     printf("Hasil: %d", calc(a,b,op));
+
+    printf("Difa sinus: %f\n", sinus(a));
+    printf("Difa sinus: %f\n", cosinus(a));
+    printf("Difa sinus: %f\n", tangent(a));
 }
 
 int calc(int a, int b, char op){

4d4cc3afcb03e27719b32e10e7a4a46c1321b77a
thisisjackii <jaxsix06@gmail.com>
2023-02-13 08:10:54 +0700

    Memindahkan fungsi

--- jacky.cpp
+++ jacky.cpp
@@ -1,1 +1,13 @@
+#include <stdio.h>
 #include "jacky.h"
+#include "difa.h"
+
+void jacky(float *a, float *b, char *op){
+    printf("\nmasukkan sebuah operasi : ");
+    scanf("%f%c%f", a, op, b);
+    // printf("Hasil: %d", calc(*a,*b,*op));
+
+   	printf("\nHasil sinus difa : %f", sinus(*a));
+    printf("\nHasil sinus difa : %f", cosinus(*a));
+    printf("\nHasil sinus difa : %f", tangent(*a));
+}

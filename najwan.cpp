ba8b4220c20816737fde7b2518e57358db15ff1e
najwanzaky <najwanzaky21@gmail.com>
2023-02-13 12:16:45 +0700

    memperbaiki function setelah konsultasi ke dosen

--- najwan.cpp
+++ najwan.cpp
@@ -4,39 +4,64 @@
 #include "najwan.h"
 #include <stdio.h>
 
-
-
-
-
-int najwan(float a, float b, char c){	
+float penjumlahan(float a,float b){
 	float hasil;
-
-	if(c == '+'){
-		hasil = a + b;
-		printf("hasil = %g", hasil);
-	}
-	else if(c == '-'){
-		hasil = a - b;
-		printf("hasil = %g", hasil);
-	}
-	else if(c == '*'){
-		hasil = a * b;
-		printf("hasil = %g", hasil);
-	}
-	else if(c == '/'){
-		hasil = a / b;
-		printf("hasil = %g", hasil);
-	}
-	else if(c == '%'){
-		hasil = fmod(a,b);
-		printf("hasil = %g", hasil);
-	}
-	else{
-		printf("anda salah memasukan operasi hitung");
-	}
-	
-
- 	return hasil;
-	//int penjumlahan(&pertama, )
+	hasil = a + b;
+	return hasil;	
 }
+
+float pengurangan(float a,float b){
+	float hasil;
+	hasil = a - b;
+	return hasil;	
+}
+
+float perkalian(float a,float b){
+	float hasil;
+	hasil = a * b;
+	return hasil;	
+}
+
+float pembagian(float a,float b){
+	float hasil;
+	hasil = a / b;
+	return hasil;	
+}
+
+float modulus(float a, float b){
+	float hasil;
+	hasil = fmod(a,b);
+	return hasil;	
+}
+
+void najwan(){	
+	float hasil;
+	
+	switch(){
+		case '+':
+			hasil = penjumlahan(a,b);
+			
+			break;
+		case '-':
+			hasil = pengurangan(a,b);
+			
+			break;
+		case '*':
+			hasil = perkalian(a,b);
+			
+			break;
+		case '/':
+			hasil = pembagian(a,b);
+			
+			break;
+		case '%':
+			hasil = modulus(a,b);
+			
+			break;
+		default:
+			printf("anda salah memasukan operasi hitung");
+	}
+}
+
+ 	
 

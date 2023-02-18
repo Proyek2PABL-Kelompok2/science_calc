1dba273f431feb68e5e8ad56521cbb06fc0727d0
Coz <117976022+CozRangers@users.noreply.github.com>
2023-02-18 16:27:40 +0700

    modify logaritma dan akar pangkat yang tidak dapat diatur sesuai keinginan user menjadi bisa sesuai keinginan user serta add program turunan polinomial

--- vico.cpp
+++ vico.cpp
@@ -2,58 +2,69 @@
 #include <stdlib.h>
 #include <math.h>
 
-int akarKuadrat(int a)
-{
-	return sqrt(a);
-}
-
-int akarKubik(int a)
-{
-	return cbrt(a);
-}
-
-int logSepuluh(int d)
-{
-	return log10(d);
-}
+#define jmlh_pangkat 10
 
 int main()
 {
-	int a,b,c;
+	int nilai,pangkat,pangkats,c,i,koef[jmlh_pangkat+1],koefs[jmlh_pangkat] ;
+	double hasils, basis,nilais;
 	float d, hasil;
 	int total = 1;
 	
 	printf("Masukan angka = ");
-	scanf("%i", &a);
+	scanf("%i", &nilai);
 	printf("Masukan pangkatnya = ");
-	scanf("%i", &b);
+	scanf("%i", &pangkat);
 	
-	for(c=1; c<=b; c++) //Membuat perpangkatan
+	for(c=1; c<=pangkat; c++) //Membuat perpangkatan
 	{
-		total = total*a;
+		total = total*nilai;
 	}
-	printf("Hasil dari %i pangkat %i adalah = %i \n", a,b,total); //Hasil dari perpangkatan
-	
-	
-	printf("Masukan angka yang akan diakar kuadrat kan = "); //Membuat akar kuadrat
-	scanf("%i",&a);
-	
-	hasil = sqrt(a);
-	printf("Hasil dari akar kuadrat %i adalah %g \n",a,hasil); //Hasil akar kuadrat
+	printf("Hasil dari %i pangkat %i adalah = %i \n", nilai,pangkat,total); //Hasil dari perpangkatan
 
+	printf("Masukan bilangan yang akan diakarkan :"); //Membuat nilai akar
+	scanf("%i", &nilai);
+	
+	printf("Masukan nilai pangkatnya :"); //Membuat pangkat dari akarnya
+	scanf("%i", &pangkat);
+	
+	hasil = pow(nilai, 1.0/pangkat);
+	
+	printf("%.2lf pangkat dari 1/%d = %.2lf \n", nilai, pangkat, hasil); //hasil yang didapat dari akar
 
-	printf("Masukan angka yang akan diakar kubik kan = "); //Membuat akar kubik
-	scanf("%i",&a);
+	printf("Masukan angka untuk log :"); //Membuat logaritma
+	scanf("%lf", &nilais); //Memasukan nilainya
 	
-	hasil = cbrt(a);
-	printf("Hasil dari akar kubik %i adalah %g \n",a,hasil); //Hasil akar kubik
+	printf("Masukan basis log :");
+	scanf("%lf", &basis); //Memasukan basis dari log
 	
+	hasils = log(nilais)/log(basis);
+	printf("log(%.2lf) base %.2lf = %.2lf \n", nilais, basis, hasils); //Hasil logaritma
 	
-	printf("Masukan angka yang akan di log10 = "); //Membuat perhitungan dari log10
-	scanf("%f", &d);
-	
-	hasil = logSepuluh(d);
-	printf("Mengubah %g menjadi log10 adalah %g",d,hasil); //Hasil log10
-	
+    printf("Masukkan banyak variabel persamaan: "); //Membuat turunan polinomial
+    scanf("%d", &pangkats); //Memasukan jumlah dari isinya misal membuat tiga pangkat = (ax^3 + bx^2 + cx + d)
+
+    printf("Masukkan koefisien setiap pangkatnya dari 0 hingga %d: \n", pangkats);
+    for (i = 0; i <= pangkats; i++) 
+	{
+        printf("koefisien untuk pangkat %d: ", i);
+        scanf("%d", &koef[i]);
+    }
+
+    for (i = 0; i < pangkats; i++)
+	{
+        koefs[i] = koef[i+1] * (i+1);
+    }
+
+    printf("Turunannya adalah: ");
+    for (i = 0; i < pangkats; i++) 
+	{
+        printf("%dx^%d ", koefs[i], i);
+        if (i < pangkat-1) 
+		{
+            printf("+ ");
+        }
+    }
+
 	return 0;
 }

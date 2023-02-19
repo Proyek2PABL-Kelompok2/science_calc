cf72d867171c5e982aac3d877b03cf7b02b5a6c3
mdifal <mdifa1565@gmail.com>
2023-02-20 00:51:57 +0700

    [Add] Function hitungQuery, untuk kalkulator standar, -hanya bisa int positif

--- difa.cpp
+++ difa.cpp
@@ -1,6 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#include <stdio.h>
+#include <ctype.h>
 #include "difa.h"
 
 float hitungTrigono(char *query){
@@ -70,3 +72,212 @@ float atangent(float angka){
 	return atan(angka);
 }
 
+int operasi(char opp,int operandNum1, int operandNum2){
+	int hasil;
+			if(opp=='*'){
+				hasil = operandNum1 * operandNum2;
+			}
+			else if(opp=='/'){
+				hasil = operandNum1 / operandNum2;
+			}
+			else if(opp=='+'){
+				hasil = operandNum1 + operandNum2;
+			}
+			
+			else if(opp=='-'){
+				hasil = operandNum1 - operandNum2;
+			}
+	return hasil;
+	
+}
+
+int hitungQuery(char query[]){
+	char operand1[250], operand2[250];
+	int i, j;
+	char opp;
+	int hasil,operandNum1,operandNum2;
+	int iteration;
+	char temp[20];
+	int k, awal, akhir, penampung, iTemp;
+	i = 0;
+	j = 0;
+		while (strstr(query, "*") || strstr(query, "/"))
+	{
+		k = 0;
+		if (query[i] == '*'|| query[i] == '/')
+		{
+			memset(operand1,'\0',7);
+			memset(operand2,'\0',7);
+			opp = query[i]; //assign operator
+			j = i - 1;
+			while ((isdigit(query[j])||query[j]=='.' )&& j >= 0) // deteksi operand 1
+			{
+
+				operand1[k] = query[j];
+				j--;
+				k++;
+			}
+			
+			
+			awal = j + 1;
+			j = i + 1;
+			k = 0;
+			while ((isdigit(query[j])||query[j]=='.' ) && query[j] != '\0') // deteksi operand 2
+			{
+				operand2[k] = query[j];
+				j++;
+				k++;
+			}
+			akhir = j - 1;
+			strrev(operand1);
+			operandNum1 = atof(operand1);
+			operandNum2 = atof(operand2);
+			
+			hasil = operasi(opp,operandNum1,operandNum2);
+			
+			sprintf(temp, "%d", hasil);
+
+
+			penampung = awal;
+			
+			while (awal <= akhir) // done
+			{
+				query[awal] = '\0';
+				awal++;
+			}
+
+			awal = penampung;
+
+			iTemp = 0;
+			while (temp[iTemp] != '\0') // loop jika temp tidak null
+			{
+				query[awal] = temp[iTemp]; // assign isi temp ke query dengan indeks var awal
+				iTemp++;
+				awal++;
+			}
+
+			awal = penampung;
+			penampung = akhir;
+			int iawal = awal;
+			int iakhir = akhir;
+			// dibawah adalah untuk shift query
+			while (awal <= akhir)
+			{
+				if (query[iawal] == '\0')
+				{
+					while (query[iakhir + 1] != '\0')
+					{
+
+						query[iawal] = query[iakhir + 1];
+						query[iawal + 1] = '\0';
+						iawal++;
+
+						iakhir++;
+					}
+					
+				}
+				
+				iawal++;
+				awal++;
+			}
+
+		}
+		else{
+			i++;
+		}
+		
+		
+	}
+	i = 0;
+	j = 0;
+	while (strstr(query, "+") || strstr(query, "-"))
+	{
+		k = 0;
+		if (query[i] == '+' || (query[i] == '-' && i>=0))
+		{
+			memset(operand1,'\0',7);
+			memset(operand2,'\0',7);
+			opp = query[i]; //assign operator
+
+			j = i - 1;
+			while ((isdigit(query[j])||query[j]=='.' ) && j >= 0) // deteksi operand 1
+			{
+
+				operand1[k] = query[j];
+				j--;
+				k++;
+			}
+			
+			
+			awal = j + 1;
+			j = i + 1;
+			k = 0;
+			while ((isdigit(query[j])||query[j]=='.' ) && query[j] != '\0') // deteksi operand 2
+			{
+				operand2[k] = query[j];
+				j++;
+				k++;
+			}
+			akhir = j - 1;
+			strrev(operand1);
+
+			
+			operandNum1 = atof(operand1);
+			operandNum2 = atof(operand2);
+			
+			hasil = operasi(opp,operandNum1,operandNum2);
+			
+			sprintf(temp, "%d", hasil);
+
+
+			penampung = awal;
+		
+			while (awal <= akhir)
+			{
+				query[awal] = '\0';
+				awal++;
+			}
+
+			awal = penampung;
+
+			iTemp = 0;
+			while (temp[iTemp] != '\0') // loop jika temp tidak null
+			{
+				query[awal] = temp[iTemp]; // assign isi temp ke query dengan indeks var awal
+				iTemp++;
+				awal++;
+			}
+
+			awal = penampung;
+			penampung = akhir;
+			int iawal = awal;
+			int iakhir = akhir;
+			// dibawah adalah untuk shift query
+			while (awal <= akhir)
+			{
+				if (query[iawal] == '\0')
+				{
+					while (query[iakhir + 1] != '\0')
+					{
+
+						query[iawal] = query[iakhir + 1];
+						query[iawal + 1] = '\0';
+						iawal++;
+
+						iakhir++;
+					}
+					
+				}
+				
+				iawal++;
+				awal++;
+			}
+
+		}
+		else{
+			i++;
+		}
+		}
+		hasil = atof(query);
+		return hasil;
+}

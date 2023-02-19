6fca109bed4de8f355ea820632458af1baafef81
fathiaqurrata <fathiaqurrataaini@gmail.com>
2023-02-19 19:14:50 +0700

    [add] deret aritmatika dan deret geometri

--- fathia.cpp
+++ fathia.cpp
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
-#include <math.h>
 #define MAX_SIZE 10
-#include "fathia.h"
-#include "najwan.h"
+#include <math.h>
+//#include "fathia.h"
+//#include "najwan.h"
 
-
+// deklarasi
 	int size;
-    float matrix1[MAX_SIZE][MAX_SIZE], matrix2[MAX_SIZE][MAX_SIZE], hasil[MAX_SIZE][MAX_SIZE];
+    float matrix1[MAX_SIZE][MAX_SIZE], matrix2[MAX_SIZE][MAX_SIZE], hasil[MAX_SIZE][MAX_SIZE], A[MAX_SIZE][MAX_SIZE];
     int option;
     int i, j, k;
-    float det1,det2,in1,in2;
+    float det1, det2, det3, det4, in1,in2;
 
 
     
-    
+// memasukan angka yang ingin dimasukan   
 void input(float matrix[][MAX_SIZE], int size) {
     int i, j;
     for (i = 0; i < size; i++) {
         for (j = 0; j < size; j++) {
-            printf("Enter the element at row %d, column %d: ", i+1, j+1);
+            printf("Angka untuk baris %d, kolom %d: ", i+1, j+1);
             scanf("%f", &matrix[i][j]);
         }
     }
 }
 
+// menampilkan nilai dari operasi matriks
 void tampil(float matrix[][MAX_SIZE], int size) {
     int i, j;
     for (i = 0; i < size; i++) {
@@ -36,7 +36,7 @@ void tampil(float matrix[][MAX_SIZE], int size) {
     }
 }
 
-
+// memilih menu operasi mtriks
 void pilihan(){ //fungsi pilihan menu perhitungan
     printf("\n[1] Penjumlahan"); 
     printf("\n[2] Pengurangan");
@@ -46,20 +46,16 @@ void pilihan(){ //fungsi pilihan menu perhitungan
     printf("\n[0] Keluar");
 }
 
-//	
-//void tambah(float matrix1[][MAX_SIZE], float matrix2[][MAX_SIZE], float hasil[][MAX_SIZE], int size){
-//}
-//
-//void kurang(float matrix1[][MAX_SIZE], float matrix2[][MAX_SIZE], float hasil[][MAX_SIZE], int size) {
-//}
-//
-//void kali(float matrix1[][MAX_SIZE], float matrix2[][MAX_SIZE], float hasil[][MAX_SIZE], int size) {
-//}
-//
-//void determinan(float matrix1[][MAX_SIZE], float matrix2[][MAX_SIZE], float det1, float det2, int size) {
-//}
-//void invers(float matrix1[][MAX_SIZE], float matrix2[][MAX_SIZE], float det1, float det2,float in1, float in2, int size) {
-//}
+	
+void tambah(float matrix1[][MAX_SIZE], float matrix2[][MAX_SIZE], float hasil[][MAX_SIZE], int size){
+}
+
+void kurang(float matrix1[][MAX_SIZE], float matrix2[][MAX_SIZE], float hasil[][MAX_SIZE], int size) {
+}
+
+void kali(float matrix1[][MAX_SIZE], float matrix2[][MAX_SIZE], float hasil[][MAX_SIZE], int size) {
+}
+
 
 
 
@@ -70,6 +66,11 @@ int main() {
     printf("pilih ordo matriks: ");
     scanf("%d", &size);
 
+ 	pilihan();
+    printf("\nMasukkan pilihan >");
+    scanf("%d",&pilih);
+    printf("%d",pilih);
+    
     printf("Masukan angka matriks pertama:\n");
     input(matrix1, size);
     printf("Matrix 1 :\n");
@@ -80,9 +81,6 @@ int main() {
     printf("Matrikx 2:\n");
     tampil(matrix2, size);
 
-    pilihan();
-    printf("\nMasukkan pilihan >");
-    scanf("%d",&pilih);
 
 
     switch (pilih) {
@@ -121,33 +119,40 @@ int main() {
 	        printf("\n Hasil:\n");
 	        tampil(hasil, size);
 	        break;
-	    
-		case 4:
-	        determinan(matrix1, matrix2, det1, det2, size); 
-	    	for (i = 0; i < size; i++) {
-		        for (j = 0; j < size; j++) {
-			        det1= ((matrix1[0][0]*matrix1[1][1])-(matrix1[0][1]*matrix1[1][0]));
-				    det2 = ((matrix2[0][0]*matrix2[1][1])-(matrix2[0][1]*matrix2[1][0]));
-				}
-			}
-			printf("\n Hasil: %d, %d\n", det1,det2);
-	    	break;
-
-		case 5:
-			for (i = 0; i < size; i++) {
-		        for (j = 0; j < size; j++) {
-			        det1= ((matrix1[0][0]*matrix1[1][1])-(matrix1[0][1]*matrix1[1][0]));
-				    det2 = ((matrix2[0][0]*matrix2[1][1])-(matrix2[0][1]*matrix2[1][0]));
-				}
-			}
-		    in1 = 1/(det1);
-		    in2 = 1/(det2);
-		    printf("\n Hasil: %d, %d\n", in1,in2);
-	    	break;
 
 		default:
 	        printf("Salah memasukan angka.\n");
 	        break;
 	}
+	
+	
+	// membuat deret aritmatika dan geometri
+	    int i,awal, beda, n, rasio,hasila, hasilg;
+	    printf("Masukan nilai a: ");
+	    scanf("%d", &awal);
+	    printf("Masukan nilai b: ");
+	    scanf("%d", &beda);
+	    printf("Masukan nilai n : ");
+	    scanf("%d", &n);
+	    printf("Masukan nilai r : ");
+	    scanf("%d", &rasio);
+	    if (beda!=0){
+	   	    hasila= awal +(n-1)*beda; 
+	    }
+	    else if (beda=0){
+	    	hasila=awal;
+		}
+	    printf("Nilai Un Aritmatika= %d\n", hasila); 
+	    
+	    if (n!=0){
+		 	hasilg= awal * pow(rasio,(n-1));
+		}
+		else if(n==0){
+			hasilg= awal * pow(rasio,(0-1));
+		}
+		printf("Nilai Un Geometri= %d\n", hasilg); 
+	
+
+	
     return 0;
 }

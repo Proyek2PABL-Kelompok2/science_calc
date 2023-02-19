31ada74012a47c5472f6c6bb14886390860e7b09
Coz <117976022+CozRangers@users.noreply.github.com>
2023-02-19 21:28:12 +0700

    modify var

--- vico.cpp
+++ vico.cpp
@@ -4,14 +4,9 @@
 
 #define jmlh_pangkat 10
 
-int perpangkatan(int nilai,int pangkat)
+int akarPangkat(float nilai_akar,int pangkat_akar)
 {
-	return nilai;
-}
-
-int akarpangkat(float nilai_akar,int pangkat)
-{
-	return pow(nilai_akar, 1.0/pangkat);
+	return pow(nilai_akar, 1.0/pangkat_akar);
 } 
 
 float logaritma(double nilai_loga, double basis)
@@ -19,38 +14,38 @@ float logaritma(double nilai_loga, double basis)
 	return log(nilai_loga)/log(basis);
 }
 
-int turunanpolinomial(int pangkats, int koef)
+int turunanPolinomial(int pangkat_turunan, int koef)
 {
-	return pangkats;
+	return pangkat_turunan;
 }
 
 int main()
 {
-	int i, c, nilai, pangkat, pangkats, koef[jmlh_pangkat+1], koefs[jmlh_pangkat] ;
+	int i, c, nilai_perpangkatan, pangkat_perpangkatan, pangkat_akar, pangkat_turunan, koef[jmlh_pangkat+1], koefs[jmlh_pangkat] ;
 	double basis, nilai_loga, hasil_loga;
 	float nilai_akar, hasil_akar;
 	long long total = 1;
 	
 	printf("Masukan angka yang akan dipangkatkan= ");
-	scanf("%i", &nilai);
+	scanf("%i", &nilai_perpangkatan);
 	printf("Masukan pangkatnya = ");
-	scanf("%i", &pangkat);
+	scanf("%i", &pangkat_perpangkatan);
 	
-	for(c=1; c<=pangkat; c++) //Membuat perpangkatan
+	for(c=1; c<=pangkat_perpangkatan; c++) //Membuat perpangkatan
 	{
-		total = total*nilai;
+		total = total*nilai_perpangkatan;
 	}
-	printf("Hasil dari %i pangkat %i adalah = %lld \n", nilai,pangkat,total); //Hasil dari perpangkatan
+	printf("Hasil dari %i pangkat %i adalah = %lld \n", nilai_perpangkatan, pangkat_perpangkatan,total); //Hasil dari perpangkatan
 
 	printf("Masukan bilangan yang akan diakarkan :"); //Membuat nilai akar
 	scanf("%f", &nilai_akar);
 	
 	printf("Masukan nilai pangkatnya :"); //Membuat pangkat dari akarnya
-	scanf("%i", &pangkat);
+	scanf("%i", &pangkat_akar);
 	
-	hasil_akar=pow(nilai_akar, 1.0/pangkat);
+	hasil_akar=pow(nilai_akar, 1.0/pangkat_akar);
 	
-	printf("%.2lf pangkat dari 1/%d = %.2lf \n", nilai_akar, pangkat, hasil_akar); //hasil yang didapat dari akar
+	printf("%.2lf pangkat dari 1/%d = %.2lf \n", nilai_akar, pangkat_akar, hasil_akar); //hasil yang didapat dari akar
 
 	printf("Masukan angka untuk log :"); //Membuat logaritma
 	scanf("%lf", &nilai_loga); //Memasukan nilainya
@@ -62,25 +57,25 @@ int main()
 	printf("log(%.2lf) base %.2lf = %.2lf \n", nilai_loga, basis, hasil_loga); //Hasil logaritma
 	
     printf("Masukkan banyak variabel persamaan: "); //Membuat turunan polinomial
-    scanf("%d", &pangkats); //Memasukan jumlah dari isinya misal membuat tiga pangkat = (ax^3 + bx^2 + cx + d)
+    scanf("%d", &pangkat_turunan); //Memasukan jumlah dari isinya misal membuat tiga pangkat = (ax^3 + bx^2 + cx + d)
 
-    printf("Masukkan koefisien setiap pangkatnya dari 0 hingga %d: \n", pangkats);
-    for (i = 0; i <= pangkats; i++) 
+    printf("Masukkan koefisien setiap pangkatnya dari 0 hingga %d: \n", pangkat_turunan);
+    for (i = 0; i <= pangkat_turunan; i++) 
 	{
         printf("koefisien untuk pangkat %d: ", i);
         scanf("%d", &koef[i]);
     }
 
-    for (i = 0; i < pangkats; i++)
+    for (i = 0; i < pangkat_turunan; i++)
 	{
         koefs[i] = koef[i+1] * (i+1);
     }
 
     printf("Turunannya adalah: ");
-    for (i = 0; i < pangkats; i++) 
+    for (i = 0; i < pangkat_turunan; i++) 
 	{
         printf("%dx^%d ", koefs[i], i);
-        if (i < pangkats-1) 
+        if (i < pangkat_turunan-1) 
 		{
             printf("+ ");
         }

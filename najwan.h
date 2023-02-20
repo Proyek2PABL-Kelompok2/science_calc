a634307a1bf45de945977f727977f62a2bee4e38
thisisjackii <jaxsix06@gmail.com>
2023-02-20 07:49:24 +0700

    [Fix] Conflicts with version control software intruding source codes

--- najwan.h
+++ najwan.h
@@ -9,9 +9,5 @@ float pengurangan(float a,float b);
 float perkalian(float a,float b);
 float pembagian(float a,float b);
 float modulus(float a, float b);
-<<<<<<< HEAD
-#endif
-=======
 
 #endif
->>>>>>> 8e70f6488820bba3c42a21af75aa69141424207c

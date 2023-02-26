f2650dffc1b8ffb49a411b33bed7626ce81cf3bc
mdifal <mdifa1565@gmail.com>
2023-02-27 00:28:19 +0700

    [ADD] add akar dan pangkat pada kalkulaator inline

--- difa.cpp
+++ difa.cpp
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <ctype.h>
 #include "difa.h"
+#include "najwan.h"
+#include "vico.h"
 
 float hitungTrigono(char *query){
 int angka;
@@ -80,18 +82,24 @@ float atangent(float angka){
 
 float operasi(char opp,float operandNum1, float operandNum2){
 	float hasil;
-			if(opp=='*'){
-				hasil = operandNum1 * operandNum2;
+			if (opp=='^'){
+				hasil=pow(operandNum1,operandNum2);
+			}
+			else if (opp=='v'){
+				hasil=akarPangkat(operandNum2,operandNum1);
+			}
+			else if(opp=='*'){
+				hasil = perkalian(operandNum1,operandNum2);
 			}
 			else if(opp=='/'){
-				hasil = operandNum1 / operandNum2;
+				hasil = pembagian(operandNum1,operandNum2);
 			}
 			else if(opp=='+'){
-				hasil = operandNum1 + operandNum2;
+				hasil=penjumlahan(operandNum1,operandNum2);
 			}
 			
 			else if(opp=='-'){
-				hasil = operandNum1 - operandNum2;
+				hasil = pengurangan(operandNum1,operandNum2);
 			}
 	return hasil;
 	
@@ -107,6 +115,155 @@ float hitungQuery(char query[]){
 	int k, awal, akhir, penampung, iTemp;
 	i = 0;
 	j = 0;
+		while (strstr(query, "^") || strstr(query, "v"))
+	{
+		
+		k = 0;
+		if (query[i] == '^' || (query[i] == 'v' && i>=0))
+		{
+			memset(operand1,'\0',250);
+			memset(operand2,'\0',250);
+			
+			opp = query[i]; //assign operator
+				
+			j = i - 1;
+		
+			while ((isdigit(query[j])||query[j]=='.' ) && j >= 0) // deteksi operand 1
+			{
+				
+				operand1[k] = query[j];
+				j--;
+				k++;
+			}
+			
+			
+			awal = j + 1;
+			j = i + 1;
+			k = 0;
+			while ((isdigit(query[j])||query[j]=='.' ) && query[j] != '\0') // deteksi operand 2
+			{
+				operand2[k] = query[j];
+				j++;
+				k++;
+			}
+			akhir = j - 1;
+			strrev(operand1);
+		
+		
+			
+			operandNum1 = atof(operand1);
+			operandNum2 = atof(operand2);
+			
+			hasil = operasi(opp,operandNum1,operandNum2);
+			
+			sprintf(temp, "%f", hasil);
+		
+
+			
+		
+			int penampung_akhir;
+			
+
+			
+			penampung_akhir = akhir;
+			
+			
+			int panjangTemp,panjangQuery,rangeOperasi,afterOperasi,penSize,iShift;
+			iShift=0;
+			panjangQuery=strlen(query);
+			penSize=panjangQuery;
+				rangeOperasi=akhir-awal+1;
+				afterOperasi=panjangQuery-akhir-1;
+			panjangTemp=strlen(temp);
+	
+			
+			while (panjangTemp-1>rangeOperasi){
+				rangeOperasi=akhir-awal+1;
+				iShift=0;
+				while(iShift<afterOperasi){
+					query[panjangQuery]=query[panjangQuery-1];
+					query[panjangQuery-1]='=';
+					panjangQuery--;
+					iShift++;	
+				}
+				panjangQuery=penSize+1;
+				penSize++;
+				akhir++;
+			}
+			i=0;
+			penampung = awal;
+						while (awal <= akhir)
+			{
+				query[awal] = '=';
+				awal++;
+			}
+		
+			awal = penampung;
+			int iawal,iakhir,space;
+			space=akhir;
+	
+			akhir=penampung_akhir;
+			int index;
+		
+			
+		
+
+		penampung=awal;
+			
+
+			iTemp = 0;
+			while (temp[iTemp] != '\0') // loop jika temp tidak null
+			{
+				query[awal] = temp[iTemp]; // assign isi temp ke query dengan indeks var awal
+				iTemp++;
+				awal++;
+			}
+			
+			iShift=0;
+			while(iShift<=strlen(query)-1){
+				if(query[iShift]=='='&&query[space+1]!='\0'){
+					query[iShift]=query[space+1];
+					query[space+1]='=';
+					space++;
+				}
+				iShift++;
+				
+			}
+			
+	
+			
+			awal = penampung;
+			penampung = akhir;
+			 iawal = awal;
+			 iakhir = akhir;
+			// dibawah adalah untuk shift query
+			while (awal <= akhir)
+			{
+				if (query[iawal] == '\0')
+				{
+					while (query[iakhir + 1] != '\0')
+					{
+
+						query[iawal] = query[iakhir + 1];
+						query[iawal + 1] = '\0';
+						iawal++;
+
+						iakhir++;
+					}
+					
+				}
+				
+				iawal++;
+				awal++;
+			}
+
+		}
+		else{
+			i++;
+		}
+		}
+		i=0;
+		j=0;
 			while (strstr(query, "*") || strstr(query, "/"))
 	{
 		

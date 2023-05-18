58f8412c41a33b69c2dd1ff2100ae6ed5d9e0c3e
thisisjackii <jaxsix06@gmail.com>
2023-05-18 20:37:50 +0700

    [Modify] Deleting extraneous comments from old code and hints

--- main.cpp
+++ main.cpp
@@ -9,19 +9,6 @@
 #include "vico.h"
 
 int main() {
-    // create the leaf nodes for 2, 3, 4, 5, and 7
-//    treeNode* n2 = createNode(2, '\0', NULL, NULL, NULL);
-//    treeNode* n3 = createNode(3, '\0', NULL, NULL, NULL);
-//    treeNode* n4 = createNode(4, '\0', NULL, NULL, NULL);
-//    treeNode* n5 = createNode(5, '\0', NULL, NULL, NULL);
-//    treeNode* n7 = createNode(7, '\0', NULL, NULL, NULL);
-
-    // create the addition nodes
-//    treeNode* add1 = createNode(NULL, '+', n3, n4, NULL);
-//    treeNode* add2 = createNode(NULL, '+', n2, add1, NULL);
-//    treeNode* add3 = createNode(NULL, '*', add2, n5, NULL);
-//    treeNode* sub1 = createNode(NULL, '-', add3, n7, NULL);
-
 	treeNode* nodeInsertion = NULL;
 	treeNode* root = NULL; 
 	treeNode* current = NULL;
@@ -78,7 +65,6 @@ int main() {
 					rightChildCarrier = nodeInsertion;
 					current->right = rightChildCarrier;
 					rightChildCarrier->parent = current;
-	//				parentAndChildrenPairing(NULL, rightChildCarrier, current);
 				}
 			}
 		}
@@ -93,7 +79,6 @@ int main() {
 					root = nodeInsertion;
 					root->left = leftChildCarrier;
 					leftChildCarrier->parent = root;
-//					parentAndChildrenPairing(leftChildCarrier, NULL, root);
 				}
 				else if(root->oper!='\0' && getPrecedence(root->oper) < getPrecedence(nodeInsertion->oper))
 				{
@@ -107,17 +92,8 @@ int main() {
 					current->right->parent = rightChildCarrier;
 					rightChildCarrier->parent = current;
 					current->right = rightChildCarrier;
-//					
-//					parentAndChildrenPairing(current->right, NULL, rightChildCarrier);
-//					parentAndChildrenPairing(current->left, rightChildCarrier, current);
 				}
 			}
-//			else
-//			{
-//				root = nodeInsertion;
-//			}
-		}
-//		prevNumInput = numInput;
 		prevKeypress = keypress;
 	}
 	while(keypress!='\r');
